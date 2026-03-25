55316019bb8dfe1ec9795cd53ec055d64fec44bc
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2026-03-25 23:43:39 +0800

    feat: add script for directories

--- src/HNL_BBN_Code.jl
+++ src/HNL_BBN_Code.jl
@@ -3,4 +3,6 @@ module HNL_BBN_Code
 # Physical constants, cosmology helpers, thermal rates, and HNL physics functions.
 # To be populated incrementally as notebooks are developed.
 
+include("directories.jl")
+
 end # module
